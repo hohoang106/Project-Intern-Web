cc411e1c1767f08011fe9e537d7f91ffd90cdbb4
Your Name <hohoang106>
2021-09-14 09:07:45 +0700

    update14/9

--- js/quanlybaidang.js
+++ js/quanlybaidang.js
@@ -39,18 +39,18 @@ function rederBaiDang(baidangs) {
   var listBaiDangs = document.querySelector('#listBaiDang');
   var htmls = baidangs.map(function (baidang) {
     return `
-    <tr class="post">
+    <tr class="post ">
     <td>${baidang.ngayDang}</td>
-    <td>${baidang.tieuDe}</td>
-    <td>${baidang.noiDung}</td>
+    <td onclick="btndetail(${baidang.id})" class="font-weight-bold" data-toggle="modal" data-target="#baivietModal" style="cursor: pointer;" > ${baidang.tieuDe}</td>
+    <td onclick="btndetail(${baidang.id})" class="limit-p" > ${baidang.noiDung} </td> 
     <td><img src="${baidang.hinhAnh}" width="100px" alt=""></td>
     <td>${baidang.tacGia}</td>
     <td>
-    <button type="button" class="btn btn-dark btn-sm px-3" data-toggle="modal" data-target="#updateModal">
+    <button class="btn btn-dark" data-toggle="modal" data-target="#updateModal">
       <i class="fas fa-edit"></i>
     </button>
-    <button onclick="deleteBaiDang(${baidang.id})" type="button" class="btn btn-danger">
-    <i class="fas fa-trash mr-2"></i>
+    <button onclick="deleteBaiDang(${baidang.id})"  class="btn btn-danger">
+    <i class="fas fa-trash"></i>
   </button>
     </td>
     </tr>
@@ -58,6 +58,15 @@ function rederBaiDang(baidangs) {
     `;
   });
   listBaiDangs.innerHTML = htmls.join('');
+  let element = document.querySelectorAll(".limit-p");
+for (let i = 0; i < element.length; i++) {
+  var gioiHan = element[i].innerText;
+  if (gioiHan.length > 150) {
+    gioiHan = gioiHan.substr(0, 150) + '...';
+  }
+  document.querySelectorAll(".limit-p")[i].innerText = gioiHan;
+}
+
  
     $('#contentList').DataTable({
       // searching: false,
@@ -66,7 +75,10 @@ function rederBaiDang(baidangs) {
       bInfo: false,
       bAutoWidth: false,
       ordering: false,
-      pageLength: 5
+      pageLength: 5,
+      oLanguage: {
+        sSearch: "Tìm kiếm"
+      }
     });
 }
 function handleCreateForm() {
@@ -74,8 +86,8 @@ function handleCreateForm() {
   btnAdd.onclick = function () {
     var ngaydang = document.querySelector('input[name="ngay-dang"]').value;
     var tieude = document.querySelector('input[name="tieu-de"]').value;
-    var noidung = document.querySelector('input[name="noi-dung"]').value;
-    // var data = CKEDITOR.instances.editor1.getData();
+    // var noidung = document.querySelector('input[name="noi-dung"]').value;
+    var noidung = CKEDITOR.instances.editor1.getData();
     var hinhanh = document.querySelector('input[name="chon-anh"]').value;
     var tacgia = document.querySelector('input[name="tac-gia"]').value;
 
@@ -109,7 +121,22 @@ function handleCreateForm() {
 //     });
 
 // }
-
+function btndetail(id){
+  var option = {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+        },  
+  };
+  fetch(baiDangApi + '/' + id, option)
+      .then(function(response){
+         return response.json();
+      })
+      .then(function(post){
+        var detailBaiDang = document.getElementById('detail-bai-dang');
+        detailBaiDang.innerHTML = post.noiDung;
+      })
+  }
 
 function deleteBaiDang(id) {
   var option = {
@@ -140,4 +167,6 @@ function readURL(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+
 
